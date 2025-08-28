9873a54e4c8a5085cb327d7918b23baab84b1294
Jaden's work notebook <Jaden.hu@kautex.com>
2025-08-28 10:56:03 +0800

    python功能的练习

--- 经验/practice_func.py
+++ 经验/practice_func.py
@@ -19,3 +19,56 @@ def calculate(a, b=10, operation = '+'):
 # print(calculate(8, 3, "*"))  # 8 * 3 = 24
 # print(calculate(10, 2, "/"))  # 10 / 2 = 5.0
 # print(calculate(5, 3, "^"))  # None（不支持^）
+
+
+# lambda的联系
+def add(*args):
+    sum = 0
+    
+    for _ in args:
+        sum += _
+
+    return sum
+
+# print(add(1,2,3,4))
+# print(add(*(1,2,3)))
+
+
+# f = lambda a : a+1
+
+# print((type(f)))
+
+# print(f(2))
+
+# numbers = [1, -3, 5, -2, 4]
+
+# zhengshu = list(filter(lambda x: x>0, numbers))
+
+# sqare_all = list(map(lambda x: x*x, numbers))
+
+# evens =  list(filter(lambda x : x % 2 ==0,numbers))
+
+# print(zhengshu)
+# print(sqare_all)
+# print(evens)
+
+
+import time
+
+def timer(func):
+    def wrapper(*args , **kwargs):
+        start = time.time()  # 开始
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f'用时约{(end - start) * 1000 :.2f}毫秒')
+        return result
+    return wrapper
+
+
+@timer
+def my_sleep():
+    time.sleep(1)
+
+
+
+my_sleep()
